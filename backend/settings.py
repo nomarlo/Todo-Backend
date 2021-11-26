7e74a85c9ab24d00782061d49898e91744f13e9f
LaraArias <82562682+LaraArias@users.noreply.github.com>
2021-11-25 23:22:36 -0600

    cambios

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'https://app-productividad-backend.herokuapp.com/'
+     'https://upbeat-borg-6fcf47.netlify.app/'
 ]
 
 django_heroku.settings(locals())
